a581442e329599437d904d045c5cadac9082b79b
zhifeng ou <zhifengou@hotmail.com>
2019-04-04 19:16:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
+
+const baseApi = 'https://api.github.com';
+const repo='zhifengou/javascript-fetch-lab';
+
 function getIssues() {
+  fetch(`https://api.github.com/repos/${repo}/issues`)
+    .then(handleErrors)
+    .then(res => res.json())
+    .then(showIssues);
 }
 
-function showIssues(json) {
+function showIssues(res) {
+  $("#issues").html(res);
 }
 
 function createIssue() {
-}
-
-function showResults(json) {
+  const title= document.getElementById("title").value;
+  const body= document.getElementById("body").value;
+  fetch(`https://api.github.com/repos/${repo}/issues`,{
+    method:'post',
+    title: title,
+    body: body,
+    headers: {
+      Authorization: `token ${getToken}`
+    }
+  }).then(handleErrors)
+  .then(getIssues);
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const learnRepo = 'learn-co-curriculum/javascript-fetch-lab';
+  console.log("this updates");
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${learnRepo}/forks`, {
+    method: 'post',
+    headers:{
+      Authorization: `token ${getToken()}`
+    }
+  }).then(handleErrors)
+  .then(res=>res.json()
+  .then(json=> showForkedRepo));
+}
+
+function showForkedRepo(res){
+  $("#results").html(`<a href="${res.html_url}">${res.full_name}</a>`);
+}
+
+function handleErrors(res){
+  if(!res.ok){
+    throw Error(response.statusText);
+  }
+  console.log("Response ok");
+  return res;
 }
 
 function getToken() {
